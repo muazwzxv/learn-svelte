c6ca3e96ebddeb2486d15ede45f6f817e1713458
Muash <muazwazir@gmail.com>
2025-06-05 01:51:37 +0800

    feat: I learn something about runes and svelte state

--- svelte-proj/src/lib/components/DisplayName.svelte
+++ svelte-proj/src/lib/components/DisplayName.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
-	let name = 'World';
+	let firstName = $state('Muaz');
+	let lastName = $state('Wazir');
+	let fullName = $derived(`${firstName} ${lastName}`);
+	let userName = $state('bigDihhMuaz');
+
 	let src = 'https://picsum.photos/250/300';
 	let bio = 'Hello, <b> Your mom choking on my dihh </b>';
+
+	$effect(() => {
+		if (userName || fullName) {
+			console.log('User has a username or a full name');
+		}
+	});
 </script>
 
-<img {src} alt="A photo of {name}" />
-<h1>Hello {name.toUpperCase()}</h1>
+<img {src} alt="A photo of {fullName}" />
+<h1>Hello {userName || fullName}</h1>
+
+<input
+	placeholder="First version of binding input to state"
+	value={userName}
+	oninput={(e) => {
+		// logic executed upon event fired
+		userName = e.currentTarget.value;
+	}}
+/>
+
+<input placeholder="Second version of binding input to state" bind:value={userName} />
 
 <!-- Bro got to trust the html -->
 <p>{@html bio}</p>
