55cd4190df8e65bf739bf571f70247181b347d21
Muash <muazwazir@gmail.com>
2025-06-10 00:42:08 +0800

    watafak

--- svelte-proj/src/lib/components/RandomNumber.svelte
+++ svelte-proj/src/lib/components/RandomNumber.svelte
@@ -1,21 +1,40 @@
 <script lang="ts">
+	import { untrack } from 'svelte';
+
 	let randomNumber = $state(Math.floor(Math.random() * 10));
 	let doubleRandomNumber = $derived(randomNumber * 2);
 	let staleDoubleRandomNumber = $state();
+	let history: number[] = $state([]);
+	let badExampleHistory: number[] = $state([]);
 	$effect(() => {
 		// this will be stale as value won't be undapted syncrhonously
 		// state updating is offloaded to macrotask queue
 		staleDoubleRandomNumber = randomNumber * 2;
+
+		// can cause infinite loop
+		// history.push(randomNumber);
+
+		// untrack prevents infinite loop due to state keep being updated
+		// but this means that history will only be updated when the component
+		// mounts to the DOM
+		untrack(() => {
+			// unelss we do this wtf
+			randomNumber;
+			badExampleHistory.push(randomNumber);
+		});
 	});
 </script>
 
 <div>
 	<h2>The random number is: {randomNumber}</h2>
 	<h2>Double random number is: {doubleRandomNumber}</h2>
+	<p>History: {history}</p>
 
 	<button
 		onclick={() => {
 			randomNumber = Math.floor(Math.random() * 10);
+			// this is better, you don't need $effect for this
+			history.push(randomNumber);
 			console.log({ randomNumber, doubleRandomNumber, staleDoubleRandomNumber });
 		}}>Generate</button
 	>
